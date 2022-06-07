e938205f3ba1de0bf3954445672dd4063177a774
Leonardo Chuello <ljchuello@gmail.com>
2022-06-06 21:00:36 -0500

    .

--- CockroachDbMvc/Form1.Designer.cs
+++ CockroachDbMvc/Form1.Designer.cs
@@ -28,12 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBaseDatos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContrasenia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ddlTabla = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(610, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.ddlTabla);
+            this.tabPage1.Controls.Add(this.btnConectar);
+            this.tabPage1.Controls.Add(this.txtPort);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.txtBaseDatos);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.txtContrasenia);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtUsuario);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtServidor);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(602, 398);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Configuración";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(15, 235);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(284, 23);
+            this.btnConectar.TabIndex = 10;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(15, 206);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(284, 23);
+            this.txtPort.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Puerto";
+            // 
+            // txtBaseDatos
+            // 
+            this.txtBaseDatos.Location = new System.Drawing.Point(15, 162);
+            this.txtBaseDatos.Name = "txtBaseDatos";
+            this.txtBaseDatos.Size = new System.Drawing.Size(284, 23);
+            this.txtBaseDatos.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Base Datos";
+            // 
+            // txtContrasenia
+            // 
+            this.txtContrasenia.Location = new System.Drawing.Point(15, 118);
+            this.txtContrasenia.Name = "txtContrasenia";
+            this.txtContrasenia.Size = new System.Drawing.Size(284, 23);
+            this.txtContrasenia.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(15, 74);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(284, 23);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(15, 30);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(284, 23);
+            this.txtServidor.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 398);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ddlTabla
+            // 
+            this.ddlTabla.FormattingEnabled = true;
+            this.ddlTabla.Location = new System.Drawing.Point(305, 30);
+            this.ddlTabla.Name = "ddlTabla";
+            this.ddlTabla.Size = new System.Drawing.Size(284, 23);
+            this.ddlTabla.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(305, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tabla / Vista";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(629, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button btnConectar;
+        private TextBox txtPort;
+        private Label label5;
+        private TextBox txtBaseDatos;
+        private Label label4;
+        private TextBox txtContrasenia;
+        private Label label3;
+        private TextBox txtUsuario;
+        private Label label2;
+        private TextBox txtServidor;
+        private Label label1;
+        private TabPage tabPage2;
+        private Label label6;
+        private ComboBox ddlTabla;
     }
 }
--- CockroachDbMvc/Form1.cs
+++ CockroachDbMvc/Form1.cs
@@ -1,3 +1,8 @@
+using System.Data;
+using System.Globalization;
+using Newtonsoft.Json;
+using Npgsql;
+
 namespace CockroachDbMvc
 {
     public partial class Form1 : Form
@@ -5,6 +10,69 @@ namespace CockroachDbMvc
         public Form1()
         {
             InitializeComponent();
+            Text = "CockroachDb - Mvc";
+
+            // Deshabilitamos el error multiTask
+            CheckForIllegalCrossThreadCalls = false;
+
+            // Cultura chupistica
+            var culture = CultureInfo.GetCultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+
+            // Existencia
+            if (File.Exists(@"D:\CockroachDB.json"))
+            {
+                NpgsqlConnectionStringBuilder npgsqlConnectionStringBuilder = JsonConvert.DeserializeObject<NpgsqlConnectionStringBuilder>(File.ReadAllText(@"D:\CockroachDB.json")) ?? new NpgsqlConnectionStringBuilder();
+                txtUsuario.Text = npgsqlConnectionStringBuilder.Username;
+                txtContrasenia.Text = npgsqlConnectionStringBuilder.Password;
+                txtBaseDatos.Text = npgsqlConnectionStringBuilder.Database;
+                txtServidor.Text = npgsqlConnectionStringBuilder.Host;
+                txtPort.Text = $"{npgsqlConnectionStringBuilder.Port}";
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NpgsqlConnectionStringBuilder npgsqlConnectionStringBuilder = new NpgsqlConnectionStringBuilder();
+                npgsqlConnectionStringBuilder.Host = txtServidor.Text;
+                npgsqlConnectionStringBuilder.Port = Convert.ToInt32(txtPort.Text);
+                npgsqlConnectionStringBuilder.SslMode = SslMode.Require;
+                npgsqlConnectionStringBuilder.Username = txtUsuario.Text;
+                npgsqlConnectionStringBuilder.Password = txtContrasenia.Text;
+                npgsqlConnectionStringBuilder.Database = txtBaseDatos.Text;
+                npgsqlConnectionStringBuilder.TrustServerCertificate = true;
+
+                List<string> tablas = new List<string>();
+
+
+                using (NpgsqlConnection db = new NpgsqlConnection(npgsqlConnectionStringBuilder.ToString()))
+                {
+                    db.Open();
+                    NpgsqlCommand npgsqlCommand = new NpgsqlCommand();
+                    npgsqlCommand.Connection = db;
+                    npgsqlCommand.CommandType = CommandType.Text;
+                    npgsqlCommand.CommandText = "SHOW TABLES;";
+                    using (NpgsqlDataReader npgsqlDataReader = npgsqlCommand.ExecuteReader())
+                    {
+                        while (npgsqlDataReader.Read())
+                        {
+                            tablas.Add(Convert.ToString(npgsqlDataReader["table_name"]));
+                        }
+                    }
+                }
+
+                tablas = tablas.OrderBy(x => x).ToList();
+
+                ddlTabla.DataSource = tablas;
+
+                File.WriteAllText(@"D:\CockroachDB.json", JsonConvert.SerializeObject(npgsqlConnectionStringBuilder));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
         }
     }
 }
